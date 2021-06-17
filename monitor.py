014f241a4309452b9715f0aaa44b82d2dda28cf7
k.poteryev <k.poteryev@kbsp.loc>
2021-06-17 12:14:31 +0300

    Реализована навигация и zoom

--- monitor.py
+++ monitor.py
@@ -2,14 +2,38 @@ import getpass
 import psutil
 
 
-def userName(string):
-    strings = string.split('\\')
-    return strings[-1]
-
-
 class Processes:
 
-    def __iter__(self):
+    processes = [{}]
+    needRevers = False
+
+    def __init__(self, parent=None):
+        self.currentComp = lambda p: p['pid']
+
         for process in psutil.process_iter():
-            # if getpass.getuser() == userName(process.username()):
+            self.processes.append({'pid': process.pid,
+                                   'name': process.name(),
+                                   'user': process.username()}
+                                  )
+
+    def setSorter(self, comp, reverse):
+        self.currentComp = comp
+        self.needRevers = reverse()
+        self.update()
+
+    def update(self):
+        self.processes.clear()
+        for process in psutil.process_iter():
+            self.processes.append({'pid': process.pid,
+                                   'name': process.name(),
+                                   'user': process.username()}
+                                  )
+
+        self.processes = sorted(self.processes,
+                                key=self.currentComp,
+                                reverse=self.needRevers)
+
+    def __iter__(self):
+        for process in self.processes:
+            # if getpass.getuser() == process['user'].split('\\')[-1]:
                 yield process
--- monitor_gui.py
+++ monitor_gui.py
@@ -2,20 +2,35 @@ import tkinter as tk
 import tkinter.ttk as ttk
 
 
-def sortFunc(tv, column, comp, reverse):
-    tempList = [(tv.set(k, column), k) for k in tv.get_children('')]
-    tempList = sorted(tempList, key=comp, reverse=reverse())
+# def sortFunc(tv, column, comp, reverse):
+#     tempList = [(tv.set(k, column), k) for k in tv.get_children('')]
+#     tempList = sorted(tempList, key=comp, reverse=reverse())
+#
+#     for index, (val, k) in enumerate(tempList):
+#         tv.move(k, '', index)
 
-    for index, (val, k) in enumerate(tempList):
-        tv.move(k, '', index)
+
+def getByPid(tv, pid):
+    for item in tv.get_children(''):
+        if pid == int(tv.set(item, 0)):
+            return item
+    return None
+
+
+def getRowNumber(tv, item):
+
+    for count, i in enumerate(tv.get_children('')):
+        if i == item[0]:
+            return count
+    return None
 
 
 def pidExists(tv, pid):
 
     tempList = []
     for k in tv.get_children(''):
-        if int(tv.set(k, 1)) == pid:
-            tempList.append(int(tv.set(k, 1)))
+        if int(tv.set(k, 0)) == pid:
+            tempList.append(int(tv.set(k, 0)))
 
     if len(tempList) > 0:
         return True
@@ -23,37 +38,55 @@ def pidExists(tv, pid):
         return False
 
 
+def inScrollArea(tv, row):
+    style = ttk.Style(tv)
+    rowsInTable = float(len(tv.get_children('')))
+    rowHeight = float(style.lookup('Treeview', 'rowheight'))
+    border = tv.yview()
+    lowerBorder = rowsInTable * rowHeight * float(border[0])
+    upperBorder = rowsInTable * rowHeight * float(border[1])
+    rowPos = row * rowHeight
+
+    if rowPos >= upperBorder or rowPos <= lowerBorder:
+        return False
+    else:
+        return True
+
+
 class ProcessTable(tk.Frame):
 
-    size = 0
+    processes = None
 
-    sorter = {
-        '#1': lambda row: int(row[0]),
-        '#2': lambda row: int(row[0]),
-        '#3': lambda row: str(row[0]).lower(),
-        '#4': lambda row: str(row[0]).lower()
+    comp = {
+        '#1': lambda process: process['pid'],
+        '#2': lambda process: process['name'].lower(),
+        '#3': lambda process: process['user'].lower()
     }
 
     column = {
-        '#1': 0,
-        '#2': 1,
-        '#3': 2,
-        '#4': 3
+        '#1': 1,
+        '#2': 2,
+        '#3': 3
     }
 
     reverser = {
         '#1': False,
         '#2': False,
-        '#3': False,
-        '#4': False
+        '#3': False
     }
 
-    header = ['Number', 'Pid', 'Name', 'User']
+    header = ['Pid', 'Name', 'User']
 
     def __init__(self, parent=None):
 
         super().__init__(parent)
+        self.newSorter = False
+
         self.table = ttk.Treeview(self, show="headings", selectmode="browse")
+        style = ttk.Style(self.table)
+        style.configure("Treeview.Heading", font=(None, 15), rowheight=40)
+        style.configure("Treeview", font=(None, 15), rowheight=40)
+
         self.table["columns"] = self.header
         self.table["displaycolumns"] = self.header
 
@@ -67,6 +100,9 @@ class ProcessTable(tk.Frame):
         self.table.pack(expand=tk.YES, fill=tk.BOTH)
 
         self.table.bind("<ButtonRelease-1>", self.OnReleaseCallback)
+        self.table.bind("<Control-MouseWheel>",  self.OnWheelCallback)
+        self.table.bind("<KeyPress>", self.OnKeyCallback)
+        self.table.bind("<<TreeviewSelect>>", self.ItemChangedCallback)
 
     def insert(self, rows):
         self.table.insert('', tk.END, values=rows)
@@ -74,6 +110,18 @@ class ProcessTable(tk.Frame):
     def clear(self):
         for item in self.table.get_children():
             self.table.delete(item)
+
+    def OnKeyCallback(self, event):
+        for process in self.processes:
+            if process['name'].lower()[0] == event.char:
+                item = getByPid(self.table, process['pid'])
+                self.table.selection_set(item)
+                break
+
+    def ItemChangedCallback(self, event):
+        row = getRowNumber(self.table, self.table.selection())
+        if not inScrollArea(self.table, row):
+            self.table.yview_moveto(row / len(self.table.get_children('')))
 
     def OnReleaseCallback(self, event):
         region = self.table.identify("region", event.x, event.y)
@@ -88,23 +136,54 @@ class ProcessTable(tk.Frame):
                     self.reverser[column] = True
                     return False
 
-            sortFunc(self.table, self.column[column], self.sorter[column], checkReverse)
+            self.processes.setSorter(self.comp[column],
+                                     checkReverse
+                                     )
 
-    def update(self, ps):
-        ''' сохранить актуальные '''
+            self.newSorter = True
+            self.table.yview_moveto(-1)
+
+    def OnWheelCallback(self, event):
+        style = ttk.Style(self.table)
+        strFont = style.lookup('Treeview', 'font')
+        currentFont = [int(s) for s in strFont.split() if s.isdigit()]
+
+        if event.delta > 0 and not currentFont[-1] > 20:
+            currentFont[-1] += 1
+        if event.delta < 0 and not currentFont[-1] < 15:
+            currentFont[-1] -= 1
+
+        style.configure("Treeview.Heading", font=(None, currentFont[-1]), rowheight=40)
+        style.configure("Treeview", font=(None, currentFont[-1]), rowheight=40)
+
+    def updateTable(self):
+        self.processes.update()
         currentPidList = []
 
-        for p in ps:
-            currentPidList.append(p.pid)
-            if not pidExists(self.table, p.pid):
-                self.size += 1
-                self.insert((self.size, p.pid, p.name(), p.username()))
+        if self.newSorter:
+            self.clear()
+            for process in self.processes:
+                self.insert((process['pid'],
+                             process['name'],
+                             process['user']
+                             ))
+        else:
+            ''' сохранить актуальные '''
+            for process in self.processes:
+                currentPidList.append(process['pid'])
+                if not pidExists(self.table, process['pid']):
+                    self.insert((process['pid'],
+                                 process['name'],
+                                 process['user']
+                                 ))
 
-        ''' убить устаревшие '''
-        for child in self.table.get_children(''):
-            pid = self.table.set(child, 1)
-            if len([p for p in currentPidList if int(pid) == p]) == 0:
-                self.table.delete(child)
+            ''' убить устаревшие '''
+            for child in self.table.get_children(''):
+                pid = self.table.set(child, 0)
+                if len([p for p in currentPidList if int(pid) == p]) == 0:
+                    self.table.delete(child)
+
+        self.newSorter = False
 
 
 class MainWindow:
@@ -115,8 +194,8 @@ class MainWindow:
         self.window = tk.Tk()
         self.window.title(self.title)
         self.table = ProcessTable(self.window)
+        self.table.processes = processes
         self.table.pack(expand=tk.YES, fill=tk.BOTH)
-        self.processes = processes
         self.window.after(50, self.update)
 
     def show(self):
@@ -126,7 +205,7 @@ class MainWindow:
         return self.table
 
     def update(self):
-        self.table.update(self.processes)
+        self.table.updateTable()
         self.window.after(50, self.update)
 
 
